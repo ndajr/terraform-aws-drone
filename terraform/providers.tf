103c9bedd7c9924a3c9151e81d3653a55eb33d16
Neemias Junior <neemias.junior@gmail.com>
2019-11-16 01:26:54 +0100

    fix

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,10 @@
 terraform {
   required_version = "~> 0.12.0"
+}
 
-  backend "s3" {
-    encrypt        = true
-    bucket         = "374047294805-tfstate"
-    dynamodb_table = "374047294805-tfstate-lock"
-    region         = "eu-central-1"
-    key            = "platform/service.tfstate"
-  }
+provider "aws" {
+  region  = "eu-central-1"
+  profile = local.profile
 }
 
 provider "aws" {
